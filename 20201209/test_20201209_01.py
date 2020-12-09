c0469a16241c33bb4fa824d6f8b9f0613a8b1003
ouyangwuhai <303423249@qq.com>
2020-12-09 15:25:41 +0800

    20201209

---
+++ 20201209/test_20201209_01.py
@@ -0,0 +1,9 @@
+
+class Testa:
+    def test_a(self):
+        assert 1 == 1
+
+    def test_b(self):
+        assert 2==3
+
+#pytest test_20201209_01.py::Testa::test_a
---
+++ 20201209/test_20201209_02.py
@@ -0,0 +1,16 @@
+import pytest
+
+def test_a():
+    assert 1 == 1
+
+
+def test_b():
+    assert 2==3
+
+#pytest test_20201209_02.py::test_b
+
+@pytest.mark.aaa
+def test_b():
+    assert 2==2
+
+#pytest -m aaa
